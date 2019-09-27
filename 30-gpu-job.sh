d2766308760f690960ffb1f8502ba4528bb650cc
Debarpan98 <48771862+Debarpan98@users.noreply.github.com>
2019-09-27 20:00:53 +0530

    Update 30-gpu-job.sh

--- 30-gpu-job.sh
+++ 30-gpu-job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-erangauk-ab
-#SBATCH --gres=gpu:1              # Number of GPU(s) per node
-#SBATCH --cpus-per-task=4         # CPU cores/threads
+#SBATCH --gres=gpu:2              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=12         # CPU cores/threads
 #SBATCH --mem=32000M               # memory per node
 #SBATCH --time=0-12:00            # time (DD-HH:MM)
-#SBATCH --mail-user=fabiisele@gmail.com  # adjust this to match your email address
+#SBATCH --mail-user=dasdebarpan10@gmail.com  # adjust this to match your email address
 #SBATCH --mail-type=ALL
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 . venv/bin/activate
