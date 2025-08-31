2372b899f99ad22e6faca54299bf1ab9cdfffb3a
Debarpan Das <debarpan1998@gmail.com>
2025-08-31 13:44:14 -0600

    Update cpu-job-masks.sh

--- cpu-job-masks.sh
+++ cpu-job-masks.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=mask-generation
 #SBATCH --time=1:00:00
 #SBATCH --mem=16G
-#SBATCH --mail-user=fabiisele@gmail.com # adjust this to match your email address
+#SBATCH --mail-user=dasdebarpan10@gmail.com # adjust this to match your email address
 #SBATCH --mail-type=ALL
 . venv/bin/activate
 python generate_mask_patches.py
