442454d76a5adfeaf4b6aecdd33ae69199994367
Debarpan Das <debarpan1998@gmail.com>
2025-08-31 13:46:15 -0600

    Update cpu-job-images.sh

--- cpu-job-images.sh
+++ cpu-job-images.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=image-generation
 #SBATCH --time=06:00:00
 #SBATCH --mem=32G
-#SBATCH --mail-user=fabiisele@gmail.com # adjust this to match your email address
+#SBATCH --mail-user=dasdebarpan10@gmail.com # adjust this to match your email address
 #SBATCH --mail-type=ALL
 . venv/bin/activate
 python generate_slices.py
